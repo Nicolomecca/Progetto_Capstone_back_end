9becc4f0c4d5e8a54f1dd0d38fe3c71bbf9112f0
Nicolomecca <nico97.mecca@gmail.com>
2024-11-18 16:46:20 +0100

    congigurazione ambiente di sviluppo

---
+++ Progetto_Capstone/src/main/java/Nicolo_Mecca/Progetto_Capstone/ProgettoCapstoneApplication.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.Progetto_Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoCapstoneApplication.class, args);
+	}
+
+}
---
+++ Progetto_Capstone/src/test/java/Nicolo_Mecca/Progetto_Capstone/ProgettoCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.Progetto_Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
