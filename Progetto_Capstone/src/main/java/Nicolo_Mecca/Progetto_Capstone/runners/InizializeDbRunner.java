8363c5c6e5d63299891119bd7156799aa1e6ec91
Nicolomecca <nico97.mecca@gmail.com>
2024-11-20 15:33:50 +0100

    runner commentato

--- Progetto_Capstone/src/main/java/Nicolo_Mecca/Progetto_Capstone/runners/InizializeDbRunner.java
+++ Progetto_Capstone/src/main/java/Nicolo_Mecca/Progetto_Capstone/runners/InizializeDbRunner.java
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-package Nicolo_Mecca.Progetto_Capstone.runners;
-
-import Nicolo_Mecca.Progetto_Capstone.dto.UserDTO;
-import Nicolo_Mecca.Progetto_Capstone.entities.User;
-import Nicolo_Mecca.Progetto_Capstone.repository.UserRepository;
-import Nicolo_Mecca.Progetto_Capstone.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-@Component
-@Order(1)
-public class InizializeDbRunner implements CommandLineRunner {
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private PasswordEncoder bcrypt;
-
-    @Value("${admin.name}")
-    private String adminName;
-    @Value("${admin.surname}")
-    private String adminSurname;
-    @Value("${admin.username}")
-    private String adminUsername;
-    @Value("${admin.email}")
-    private String adminEmail;
-    @Value("${admin.password}")
-    private String adminPassword;
-
-    @Override
-    public void run(String... args) throws Exception {
-        if (!userRepository.existsByEmail(adminEmail)) {
-            createAdmin();
-            System.out.println("Admin created successfully with username: " + adminUsername);
-        }
-    }
-
-    private void createAdmin() {
-        UserDTO adminDTO = new UserDTO(
-                adminName,
-                adminSurname,
-                adminUsername,
-                adminEmail,
-                adminPassword
-        );
-
-        User admin = userService.saveAdmin(adminDTO);
-        userRepository.save(admin);
-    }
-}
-=======
 //package Nicolo_Mecca.Progetto_Capstone.runners;
 //
 //import Nicolo_Mecca.Progetto_Capstone.dto.UserDTO;
@@ -161,4 +104,3 @@ public class InizializeDbRunner implements CommandLineRunner {
 //        }
 //    }
 //}
->>>>>>> develop_branch
